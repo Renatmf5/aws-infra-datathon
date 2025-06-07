56e28b53cb18471c8f7de245f2f668d26fbf054c
renatmf5 <renato_mf5@hotmail.com>
2025-06-07 10:52:36 -0300

    init aws infra repo

---
+++ test/aws-infra-datathon.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsInfraDatathon from '../lib/aws-infra-datathon-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-infra-datathon-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsInfraDatathon.AwsInfraDatathonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
